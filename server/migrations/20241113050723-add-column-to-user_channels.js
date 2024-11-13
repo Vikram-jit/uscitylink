e3db6d58835ad65bed1ed0cc163013a73ac349d8
gurvinderappecker <gurvinder@appecker.net>
2024-11-13 18:10:06 +0530

    fixed migration issue

--- server/migrations/20241113050723-add-column-to-user_channels.js
+++ server/migrations/20241113050723-add-column-to-user_channels.js
@@ -8,15 +8,12 @@ module.exports = {
       type: Sequelize.INTEGER,  
       defaultValue: 0,       
     });
-    await queryInterface.addColumn('user_channels', 'last_message_utc', {
-      type: Sequelize.DATE, // Use Sequelize.DATE for datetime fields
-      allowNull: true, // Allow NULL since the field may not always be set initially
-    });
+  
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('user_channels', 'recieve_message_count');
     await queryInterface.removeColumn('user_channels', 'send_message_count');
-    await queryInterface.removeColumn('user_channels', 'last_message_utc');
+ 
   }
 };
